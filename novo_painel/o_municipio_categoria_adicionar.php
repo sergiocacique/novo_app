7aea526b829f10842c984ce2bcc726326fbee2f9
sergio cacique <serginhocacique@gmail.com>
2016-03-16 11:52:11 -0400

    atração categoria
    adicionar, editar, gravar, novo, listar

---
+++ novo_painel/o_municipio_categoria_adicionar.php
@@ -0,0 +1,32 @@
+<?php
+
+include ("conexao.php");
+include ("funcao.php");
+
+// A sessão precisa ser iniciada em cada página diferente
+if (!isset($_SESSION)) session_start();
+
+
+// Verifica se não há a variável da sessão que identifica o usuário
+if (!isset($_SESSION['UsuarioID'])) {
+    // Destrói a sessão por segurança
+    session_destroy();
+    // Redireciona o visitante de volta pro login
+    header("Location: login.php"); exit;
+}
+
+$sqlAdmin = mysql_query("SELECT * FROM vw_prefeitura WHERE CdPrefeitura = ".$_SESSION['PrefeituraID']." ");
+$verAdmin = mysql_fetch_array($sqlAdmin);
+
+    $Titulo = addslashes($_POST['titulo']);
+    $Acao = $_POST['acao'];
+    $DtAtualizacao = date('Y-m-d H:i:s');
+
+
+
+        $query = "INSERT INTO atracao_categoria (CdPrefeitura,CdUsuario,Nome,Acao,DtCadastro) VALUES ('".$_SESSION['PrefeituraID']."','" . $_SESSION['UsuarioID'] . "','" . $Titulo . "','" . $Acao . "','" . $DtAtualizacao . "')";
+        $verifica = mysql_query($query);
+
+
+header('Location: o_municipio_categoria.php'); exit;
+?>
---
+++ novo_painel/o_municipio_categoria_gravar.php
@@ -0,0 +1,34 @@
+<?php
+
+include ("conexao.php");
+include ("funcao.php");
+
+// A sessão precisa ser iniciada em cada página diferente
+if (!isset($_SESSION)) session_start();
+
+
+// Verifica se não há a variável da sessão que identifica o usuário
+if (!isset($_SESSION['UsuarioID'])) {
+    // Destrói a sessão por segurança
+    session_destroy();
+    // Redireciona o visitante de volta pro login
+    header("Location: login.php"); exit;
+}
+
+$sqlAdmin = mysql_query("SELECT * FROM vw_prefeitura WHERE CdPrefeitura = ".$_SESSION['PrefeituraID']." ");
+$verAdmin = mysql_fetch_array($sqlAdmin);
+
+
+    $id = $_POST['id'];
+    $Titulo = addslashes($_POST['titulo']);
+    $Acao = $_POST['acao'];
+    $DtAtualizacao = date('Y-m-d H:i:s');
+
+
+        $query = "UPDATE atracao_categoria SET Nome = '" . $Titulo . "', Acao = '".$Acao."' , DtAtualizacao = '" . $DtAtualizacao . "' WHERE id = '".$id."'";
+        $verifica = mysql_query($query);
+
+
+header('Location: o_municipio_categoria.php'); exit;
+
+?>
